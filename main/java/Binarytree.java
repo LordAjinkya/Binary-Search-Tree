7347bfebd4f3b0ce553bffb1e32a86dc480f6fa1
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-23 02:11:53 +0530

    Added:code to search particular key in tree

--- main/java/Binarytree.java
+++ main/java/Binarytree.java
@@ -17,15 +17,38 @@ public class Binarytree<K extends Comparable<K>> {
         }
         return current;
     }
-
+    //created a public method to get size of the tree
     public int getSize(){
         return this.getSizeRecursive(root);
     }
-
-    public int getSizeRecursive(Mybinarynode<K> current){
+    //method to get the size continuously
+    public int getSizeRecursive(Mybinarynode<K> current){///recursion
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
                 + this.getSizeRecursive(current.right);
     }
+    //boolean to return true if key is at current
+    public Boolean search(K key) {
+        Mybinarynode current =  searchRecursive(root,key);
+        if(key.equals(current.key))
+            return true;
+        else
+            return false;
+    }
+    //search specific key in tree
+
+    private Mybinarynode<K> searchRecursive(Mybinarynode current, K key) {
+        while (current.key != key) {
+            int compareResult = key.compareTo((K) current.key);//comparator used to check if key is equal to current key
+            if (compareResult == 0)
+                return current;
+            else if (compareResult < 0)
+                current = current.left;
+            else
+                current = current.right;
+        }
+        return current;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to binary search tree program");
     }
--- test/java/Binarytreetest.java
+++ test/java/Binarytreetest.java
@@ -33,6 +33,26 @@ public class Binarytreetest {
         int size=myBinaryTree.getSize();
         Assertions.assertEquals(13,size);
     }
+    //search particular element in the tree
+    @Test
+    public void returntrue_when_searched_element_retruns_element_from_the_addedelements(){
+        Binarytree<Integer> myBinaryTree=new Binarytree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(60);
+        myBinaryTree.add(63);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(67);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        boolean result = myBinaryTree.search(3);
+        Assertions.assertTrue(result);
+    }
 
 
 }
