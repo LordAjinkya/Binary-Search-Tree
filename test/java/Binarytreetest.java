611b0d7d86db6cc2d70879028cebe5285f34afb2
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-23 01:54:07 +0530

    Added:code to create the binary tree shown in the figure

--- test/java/Binarytreetest.java
+++ test/java/Binarytreetest.java
@@ -13,5 +13,26 @@ public class Binarytreetest {
         Assertions.assertEquals(3,size);
     }
 
+    //To check size of a tree with 13 numbers
+    @Test
+    public void return_size_when_numbers_are_added_tothe_tree(){
+        Binarytree<Integer> myBinaryTree=new Binarytree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(60);
+        myBinaryTree.add(63);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(67);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        int size=myBinaryTree.getSize();
+        Assertions.assertEquals(13,size);
+    }
+
 
 }
